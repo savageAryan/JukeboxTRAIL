e082bec9b53d680a78267301aa846e7c4285cedb
Aryan Patel <bontaromiii@gmail.com>
2026-09-18 09:58:56 +0530

    fixed the alingement problem with help of gpt , i had to use margin-left auto to make songs go to left of the songlist to give song gap to extend when hovered,tried getting a cooler gradient on songlist bg but could not will try again

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 const music = document.getElementById("music")
 const song = document.querySelector(".songs")
+
+song.addEventListener("click",()=>{
+    music.src = song.dataset.song;
+    music.play();
+})
