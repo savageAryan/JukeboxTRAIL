e12f1c7f1167025550e38f3b6533e59a16679e12
Aryan Patel <bontaromiii@gmail.com>
2026-09-19 19:07:27 +0530

    took help from gpt to get the record img rotating when music is playing and stop,it told me to use extra class selector .playing and use classlist property to add remove it from .record class, but then i had to use both .record and .record .playing when i made it pauseable later , made the play button pause and play took help from previous project code kept getting syntax wrong, added a songname div and inside a p for tect for song name, positioned and sized the div but for some reason p is not getting centred anyhow it got centred when i used tect aling centre but thn i changed height it sinked to bottom

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 const music = document.getElementById("music")
 const song = document.querySelector(".songs")
-
+const record = document.querySelector(".record")
 song.addEventListener("click",()=>{
     music.src = song.dataset.song;
     music.play();
+    record.classList.add("playing");
+})
+music.addEventListener("play" ,()=> {
+    record.classList.add("playing");
+})
+music.addEventListener("pause",()=> {
+    record.classList.remove("playing");
+})
+music.addEventListener("ended",()=> {
+    
+})
+
+const play = document.querySelector("#play")
+play.addEventListener("click",()=> {
+    if(music.paused) {
+        music.play();
+
+    } else {
+        music.pause()
+    }
 })
