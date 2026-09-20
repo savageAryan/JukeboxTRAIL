0d2270a174815ff78b0af9b948e151c176c424ee
Aryan Patel <bontaromiii@gmail.com>
2026-09-20 10:03:24 +0530

    took gpt help to centre p inside songname but I by own realised that it's not being centred cause the long text actually in rows rather than being in a straight line used white-space no wrap and actually positioned it, added animation of translate from right to left for songname p kept changing values till not very satisfied used classlist.add to add the animation when song playing,changed text content of song to be songs' relative path dataset.song,added image mask with help of gpt to blur sharp edges,

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const music = document.getElementById("music")
 const song = document.querySelector(".songs")
 const record = document.querySelector(".record")
+const songname = document.querySelector("#songname p")
+song.textContent = song.dataset.song
 song.addEventListener("click",()=>{
     music.src = song.dataset.song;
+    songname.textContent = song.textContent
     music.play();
     record.classList.add("playing");
 })
 music.addEventListener("play" ,()=> {
     record.classList.add("playing");
+    songname.classList.add("namescroll")
 })
 music.addEventListener("pause",()=> {
     record.classList.remove("playing");
+    songname.classList.remove("namescroll")
 })
 music.addEventListener("ended",()=> {
     
