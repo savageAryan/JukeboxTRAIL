37a807f8d61b0191e4a514d52cf14f8e6af7603f
Aryan Patel <bontaromiii@gmail.com>
2026-09-21 08:36:40 +0530

    used textcontent property to change name content of button song and songname p to song source will probably remove songs/ from them, instead of getting the songs from class I got the songs as an array with help of gpt using for songs.for each and migrated my code from before logic doing this was not working even though my code was right initially cause I did not remove the before code and in js if one line does not work nothing executes i relaised later, added active class to songs and when clicked the active class is added to clicked one and removed from other the array method was imp for this,tried changing play icon to pause don't know why it did not work

--- index.js
+++ index.js
@@ -1,32 +1,48 @@
 const music = document.getElementById("music")
-const song = document.querySelector(".songs")
+const songs = document.querySelectorAll(".songs")
 const record = document.querySelector(".record")
 const songname = document.querySelector("#songname p")
-song.textContent = song.dataset.song
-song.addEventListener("click",()=>{
-    music.src = song.dataset.song;
-    songname.textContent = song.textContent
-    music.play();
-    record.classList.add("playing");
-})
+const play = document.querySelector("#play")
+const playicon = play.querySelector("i")
+let songsource = "null"
+songname.textContent = songs.textContent
+console.log(songs);
+console.log(playicon)
 music.addEventListener("play" ,()=> {
     record.classList.add("playing");
     songname.classList.add("namescroll")
+    playicon.classList.remove("fa-play");
+    playicon.classList.add("fa-pause");
 })
 music.addEventListener("pause",()=> {
     record.classList.remove("playing");
     songname.classList.remove("namescroll")
+    playicon.classList.remove("fa-pause");
+    playicon.classList.add("fa-play");
 })
 music.addEventListener("ended",()=> {
     
 })
-
-const play = document.querySelector("#play")
 play.addEventListener("click",()=> {
     if(music.paused) {
         music.play();
+        
 
     } else {
-        music.pause()
+        music.pause();
+        
     }
 })
+songs.forEach(song => {
+    song.textContent = song.dataset.song
+    song.addEventListener("click",()=>{
+        songs.forEach(s => {
+            s.classList.remove("active");
+
+        });
+        song.classList.add("active");
+        songname.textContent = song.dataset.song
+        music.src = song.dataset.song
+        music.play();
+    });
+});
